d6768493eb133286a2e33ade9b2b56762994b205
christina <waichee723@gmail.com>
2020-10-16 16:53:13 +0800

    Add return to Title logic

--- app/src/main/java/com/waichee/triviaquiz02/ui/EndFragment.kt
+++ app/src/main/java/com/waichee/triviaquiz02/ui/EndFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.waichee.triviaquiz02.R
 import com.waichee.triviaquiz02.databinding.FragmentEndBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,6 +22,19 @@ class EndFragment: Fragment() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
+        setupObservers()
+
         return binding.root
+    }
+
+    private fun setupObservers() {
+        viewModel.navigateToTitleFragment.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                this.findNavController().navigate(
+                    R.id.action_endFragment_to_titleFragment
+                )
+                viewModel.navigateComplete()
+            }
+        })
     }
 }
--- app/src/main/java/com/waichee/triviaquiz02/ui/EndViewModel.kt
+++ app/src/main/java/com/waichee/triviaquiz02/ui/EndViewModel.kt
@@ -1,7 +1,22 @@
 package com.waichee.triviaquiz02.ui
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class EndViewModel @ViewModelInject constructor() :ViewModel() {
+
+    private val _navigateToTitleFragment = MutableLiveData<Boolean>()
+    val navigateToTitleFragment: LiveData<Boolean>
+        get() = _navigateToTitleFragment
+
+    fun returnToTitleButtonOnClick() {
+        _navigateToTitleFragment.value = true
+    }
+
+    fun navigateComplete() {
+        _navigateToTitleFragment.value = false
+    }
+
 }
